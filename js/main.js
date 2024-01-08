609099b7c3be0b565589e245898254ef23cf463d
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-08 13:44:45 +0100

    finished main logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('js ok')
+// * Creo una lista
+const shoppingItems = ['pomodoro','uova','formaggio','succo','prosciutto'];
+// * Prendo gli elementi che mi servono dalla pagina
+const containerElement = document.querySelector('.container');
+
+let contatore = 0;
+let message = '<ul>'
+
+// * Stampo con ciclo while
+
+
+
+while (contatore < shoppingItems.length) {
+    message += `<li>${shoppingItems[contatore]}<li>`
+    contatore++; 
+}
+
+message += '<ul>'
+
+containerElement.innerHTML = message;
