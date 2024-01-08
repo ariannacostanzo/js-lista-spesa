f8c1e02bc1a418b547546607dc81bfdafb7ce387
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-08 14:06:18 +0100

    added style

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 // * Creo una lista
-const shoppingItems = ['pomodoro','uova','formaggio','succo','prosciutto'];
+const shoppingItems = ['pomodoro','uova','formaggio','succo','prosciutto', 'lattuga', 'patatine surgelate'];
 // * Prendo gli elementi che mi servono dalla pagina
-const containerElement = document.querySelector('.container');
+const containerElement = document.querySelector('.container-lista');
 
 let contatore = 0;
 let message = '<ul>'
 
 // * Stampo con ciclo while
 
-
-
 while (contatore < shoppingItems.length) {
-    message += `<li>${shoppingItems[contatore]}<li>`
+    message += `<li>${shoppingItems[contatore]}<li><hr>`
     contatore++; 
 }
+
+// ? Fatto con ciclo for -----------------------------
+
+// for (let i = 0; i < shoppingItems.length; i++) {
+//     message += `<li>${shoppingItems[contatore]}<li>`
+// }
+
+// ? ----------------------------------------------------
+
 
 message += '<ul>'
 
